e5ffa2dfdee471f5095fa03d48fad549a213e476
Themba Kubheka <sombheshane@gmail.com>
2022-08-12 11:23:09 +0200

    Food oasis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import '.App.css';
-import Home from './component/Home';
-import Navbar from './component/Navbar';
-import {Switch, Route} from 'react-router-dom';
-import Corporate from './component/Corporate';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
       <>
       <Navbar/>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/corporate" component={Corporate} />
-      </Switch>
-      
       </>
   );
 }
---
+++ src/Navbav.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './food-word-sign.jpg';
+import './App.css';
+import nm from './hulled-coconut.jpg';
+
+const Navbar =()=>{
+
+    return (
+        <header className="Appp-header">
+            <nav className="navbar">
+                <div className="navbav-container">
+                    <ul className="navbar"> 
+                    <img src={logo}  className="App-logo" alt="logo"></img>
+                         <li><a href="#"> ABOUT US</a></li>
+                         <li><a href="#"> FAQS </a></li>
+                         <li><a href="#"> CORPORATE </a></li>
+                         <li><a href=""> QUICK FRUITS </a></li>
+                    </ul>
+                </div>
+                <div className='bg-img'>
+                <img src={nm}  className="gault" alt="logo"></img>
+        </div>
+            
+            </nav>
+        </header>
+        
+    );
+};
+
+export default Navbar;
